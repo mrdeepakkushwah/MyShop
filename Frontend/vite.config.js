950bef4de4f98d3f74acf55ca8c48d4ef1a3cfe8
Deepak Kushwah <160806589+mrdeepakkushwah@users.noreply.github.com>
2025-06-08 17:01:39 +0530

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    strictPort: true,
-    cors: {
-      origin: "http://localhost:4000", // Match your backend
-      credentials: true,
+  ...(command === "serve" && {
+    server: {
+      port: 3000,
+      open: true,
+      strictPort: true,
+      cors: {
+        origin: "http://localhost:4000", // Your backend URL
+        credentials: true,
+      },
     },
+  }),
+  build: {
+    outDir: "dist",
   },
-});
+}));
