cbc82f55e1ee4b85d1b9cc4a408ab069e8de8739
Deepak Kushwah <160806589+mrdeepakkushwah@users.noreply.github.com>
2025-07-18 17:37:00 +0530

    fix: removed broken app.css import

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./Components/MainLayout";
-import './app.css';
+// import './app.css';
+import './index.css';
+
 
 import Home from "./Pages/Home";
 import About from "./Pages/About";
